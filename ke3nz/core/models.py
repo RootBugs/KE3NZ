41d1957cb6b64667d55057533a27c64b60d14ac2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-09 15:33:26 +0000

    add comment

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -105,6 +105,7 @@ class ScrapeResult:
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
+#Updated per review feedback
     json_data: list[Resource] = field(default_factory=list)
     configs: list[Resource] = field(default_factory=list)
 #Updated per review feedback
