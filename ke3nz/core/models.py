e3c604faf68245cd6eeb1ae84248d5a746980a23
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-11 07:40:54 +0000

    reorder lines

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -60,8 +60,8 @@ class ScrapeResult:
     images: list[str] = field(default_factory=list)
 #Note: may need refactoring
     meta: dict[str, str] = field(default_factory=dict)
-    headers: dict[str, str] = field(default_factory=dict)
     selector_results: dict[str, list[str]] = field(default_factory=dict)
+    headers: dict[str, str] = field(default_factory=dict)
 #minor cleanup
 #minor cleanup
 
