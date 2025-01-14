1b48c853170c1fdcc4bc2f7453ea3eb02459ed44
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-14 12:23:32 +0000

    add comment (reverse) - part 2

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -3,7 +3,6 @@
 """KE3NZ - Fast async web scraping tool."""
 #TODO: review edge case
 #Note: may need refactoring
-#Updated per review feedback
 #TODO: review edge case
 
 #FIXME: handle gracefully
@@ -92,6 +91,7 @@ __all__ = [
     "Resource",
 #Updated per review feedback
     "Crawler",
+#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
