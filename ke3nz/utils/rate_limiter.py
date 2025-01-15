964a97747221cd0512888eedbebe14e57f70dc3e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-15 16:06:39 +0000

    add blank line

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -108,6 +108,7 @@ class RateLimiter:
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
+
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
