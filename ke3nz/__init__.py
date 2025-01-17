56a4a8081b23ee34efee2095d6a0fa918a474df5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-17 17:48:54 +0000

    add comment (minor)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -7,6 +7,7 @@
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 from ke3nz.core.models import Resource, ScrapeResult
