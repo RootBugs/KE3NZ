6d24399bffbb9c82e7b7b6e09530c8a0f6825b80
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-25 22:26:57 +0000

    add comment (reverse) (wip)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -14,7 +14,7 @@
 #TODO: review edge case
 #minor cleanup
 """Example: Crawling a website with KE3NZ."""
-#TODO: review edge case
+#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
