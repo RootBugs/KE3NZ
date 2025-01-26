5f22bf89b6aaed0b2de5c86a1391a4677c22e0bd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-26 05:09:53 +0000

    add comment (fallback)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -95,6 +95,7 @@ class RateLimiter:
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
+#Note: may need refactoring
                 await asyncio.sleep(self._min_interval - elapsed)
 
 #minor cleanup
