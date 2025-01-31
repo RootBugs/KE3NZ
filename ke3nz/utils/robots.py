76dbda6dc6693c34122b20b979d200fa2ffe2fb7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-31 14:28:47 +0000

    add comment - part 2

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -92,6 +92,7 @@ class RobotsChecker:
 #TODO: review edge case
         rp = await self._fetch_robots(url)
 #Note: may need refactoring
+#minor cleanup
         return rp.can_fetch(user_agent, url)
 #TODO: review edge case
 #Note: may need refactoring
