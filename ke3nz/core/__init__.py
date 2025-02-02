3530dbf11db602a45656bba960fe2ebbb312cd22
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-02 19:24:36 +0000

    add comment (fallback)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -85,6 +85,7 @@ import itertools
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
