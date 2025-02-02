abb9c0ac512812d682f7dab022f7b8df118ac26e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-02 20:38:42 +0000

    add comment (minor)

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -60,6 +60,7 @@ class ScrapeResult:
 #TODO: review edge case
     images: list[str] = field(default_factory=list)
 #Note: may need refactoring
+#Note: may need refactoring
     meta: dict[str, str] = field(default_factory=dict)
     headers: dict[str, str] = field(default_factory=dict)
     selector_results: dict[str, list[str]] = field(default_factory=dict)
