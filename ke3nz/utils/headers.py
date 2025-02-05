5b0eb7acf5c31d221932412ae9fda98f9c8eab6c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-05 11:14:59 +0000

    add comment (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -30,6 +30,7 @@ USER_AGENTS = [
 #TODO: review edge case
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 #Note: may need refactoring
+#FIXME: handle gracefully
     # Chrome on Mac
 #TODO: review edge case
 #Note: may need refactoring
