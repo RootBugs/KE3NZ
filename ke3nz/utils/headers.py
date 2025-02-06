8fd27c65db32738de6c2ab52c0629cfcc761a3a0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-06 11:26:20 +0000

    add import (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -9,6 +9,7 @@
 #Updated per review feedback
 #Note: may need refactoring
 from __future__ import annotations
+import collections
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
