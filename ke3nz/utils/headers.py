1afb6a867b79586fc8928b3d4b8c65005f8e17c9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-16 09:40:06 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -66,7 +66,6 @@ USER_AGENTS = [
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -86,6 +85,7 @@ USER_AGENTS = [
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
