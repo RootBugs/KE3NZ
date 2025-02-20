ccd51cc9c6085d3f8e03efbe32dc94296832bca0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-20 14:34:59 +0000

    add comment (fallback) - part 2

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -41,6 +41,7 @@ def get_random_headers() -> dict[str, str]:
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
+#TODO: review edge case
         "Upgrade-Insecure-Requests": "1",
         "Sec-Fetch-Dest": "document",
         "Sec-Fetch-Mode": "navigate",
