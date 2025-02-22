b899699cc72d4d11281c25b568c6524060043752
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-22 12:19:21 +0000

    add comment (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -62,6 +62,7 @@ USER_AGENTS = [
 #minor cleanup
 #minor cleanup
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
+#FIXME: handle gracefully
 #minor cleanup
     # Chrome on Linux
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
