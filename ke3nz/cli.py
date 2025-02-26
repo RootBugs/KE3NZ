d8fd58a512e4bab2ce4df072ee500d308ade2015
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-26 10:58:23 +0000

    add blank line

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -288,6 +288,7 @@ async def cmd_resources(args: argparse.Namespace) -> None:
 #Updated per review feedback
         if args.save:
 #FIXME: handle gracefully
+
 #Note: may need refactoring
             base = s.save_resources(value, args.save)
             if args.verbose:
