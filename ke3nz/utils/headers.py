69c8ea47bdec2669d7e6e82e3a10d1d04fe7a4c2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-28 10:50:34 +0000

    add comment (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -22,6 +22,7 @@ USER_AGENTS = [
     # Chrome on Linux
 #TODO: review edge case
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+#minor cleanup
     # Firefox on Linux
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
 #TODO: review edge case
