263d437f7b034c91d8139f366d0faca0ec78ec93
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-01 06:41:00 +0000

    add comment (fallback)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -154,6 +154,7 @@ def get_random_headers() -> dict[str, str]:
 #minor cleanup
         "Sec-Fetch-Mode": "navigate",
 #FIXME: handle gracefully
+#FIXME: handle gracefully
         "Sec-Fetch-Dest": "document",
         "Sec-Fetch-Site": "none",
         "Sec-Fetch-User": "?1",
