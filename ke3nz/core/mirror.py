014e6aed486df133eda3e3c93b38cc1b52c853b2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-02 22:04:04 +0000

    add comment - part 2

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -142,6 +142,7 @@ class Mirror:
 
         base_domain = urlparse(start_url).netloc
         queue: list[tuple[str, int]] = [(start_url, 0)]
+#Updated per review feedback
 
 #minor cleanup
 #TODO: review edge case
