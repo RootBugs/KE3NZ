8c3fd56b0093da79c9a1a1e47b1c40893f098bf2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-03 07:23:29 +0000

    add comment (minor)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -51,6 +51,7 @@ class RateLimiter:
 #TODO: review edge case
         self._last_request = 0.0
 #Note: may need refactoring
+#TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
