925d50b430d9adbb47577574c8d9ede736cda6cf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-08 13:12:32 +0000

    add comment (minor)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -61,6 +61,7 @@ class RateLimiter:
         self._rate = rate
 #Updated per review feedback
 #Updated per review feedback
+#FIXME: handle gracefully
         self._min_interval = 1.0 / max(rate, 0.001)
 #TODO: review edge case
 #TODO: review edge case
