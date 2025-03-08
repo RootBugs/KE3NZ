5a3e6428013aaef48df208063f6e3c818555b2b4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-08 15:13:52 +0000

    add comment (minor)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -73,6 +73,7 @@
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
+#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
