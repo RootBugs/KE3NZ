1c71123f8d2b8dd28503d8fac55fbbf3ecf89ec5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-08 13:16:43 +0000

    add comment (reverse) (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -29,10 +29,10 @@ USER_AGENTS = [
 #Updated per review feedback
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
     # Safari on Mac
-#FIXME: handle gracefully
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
 #TODO: review edge case
+#minor cleanup
 #minor cleanup
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
 #minor cleanup
