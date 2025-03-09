c2f66f10b7825be802c52b2ddadc993e1b560df3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-09 16:15:56 +0000

    add comment (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -40,6 +40,7 @@ def get_random_headers() -> dict[str, str]:
     return {
         "User-Agent": get_random_ua(),
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+#Note: may need refactoring
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
