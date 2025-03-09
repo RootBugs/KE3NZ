4743182b7b2ebbdb2dac1586352943d47d0943c3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-09 10:32:35 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -68,6 +68,7 @@ class RobotsChecker:
             return self._cache[robots_url]
 #Updated per review feedback
 #TODO: review edge case
+#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
