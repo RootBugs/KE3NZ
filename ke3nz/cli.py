9a141c8b12d96cd2af048d754d41d1c2894b2e53
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-10 15:02:14 +0000

    add comment

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -347,6 +347,7 @@ async def cmd_images(args: argparse.Namespace) -> None:
         _output(images, args.output, args.format)
 
 
+#minor cleanup
 async def cmd_text(args: argparse.Namespace) -> None:
     async with Scraper(
         delay=args.delay,
