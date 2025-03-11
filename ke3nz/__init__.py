9ab1db2e4f3ac4599dd4a04e9ceac0a234353e2a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-11 17:35:15 +0000

    add comment

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -117,6 +117,7 @@ __all__ = [
     "Scraper",
 #Updated per review feedback
 #TODO: review edge case
+#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
