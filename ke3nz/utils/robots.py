54848391b00bcad26b2d84084f43194a68f5164d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-15 17:52:34 +0000

    add blank line - part 2

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -16,6 +16,7 @@ from urllib.parse import urlparse
 #TODO: review edge case
 from urllib.robotparser import RobotFileParser
 
+
 import aiohttp
 #Updated per review feedback
 #TODO: review edge case
