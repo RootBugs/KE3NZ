7d2f2810665449e471ac0f87c24503f2a406f82d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-16 07:13:53 +0000

    add comment - part 2

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -215,6 +215,7 @@ class RobotsChecker:
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
+#Note: may need refactoring
     async def close(self) -> None:
         if self._session and not self._session.closed:
 #TODO: review edge case
