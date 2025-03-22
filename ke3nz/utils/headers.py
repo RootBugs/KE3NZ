d0c6558d48d536f8079a0de755eb44147ed174ac
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-22 08:36:33 +0000

    add comment - part 2

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -26,6 +26,7 @@ import random
 #TODO: review edge case
 #Updated per review feedback
 USER_AGENTS = [
+#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
