60061899bbce73a5795e9d91434d3ca71505f361
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-31 20:53:40 +0000

    add blank line

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -129,6 +129,7 @@ class TestParser:
 
         assert "https://example.com/image1.jpg" in result.images
         assert "https://example.com/image2.png" in result.images
+
         assert "https://example.com/image3-sm.jpg" in result.images
 #FIXME: handle gracefully
         assert "https://example.com/image3-lg.jpg" in result.images
