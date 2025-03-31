72e6fff3ec0fa6d5e08a61ddf08d1616d536a747
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-31 09:29:04 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -83,6 +83,7 @@ class RobotsChecker:
     async def _fetch_robots(self, url: str) -> RobotFileParser:
 #Updated per review feedback
         parsed = urlparse(url)
+#TODO: review edge case
 
 #Note: may need refactoring
 #TODO: review edge case
