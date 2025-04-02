8c3a33b526fe1c2e98eaf13013be6208d233a285
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-02 17:42:11 +0000

    add comment (fallback)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,6 +1,7 @@
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
+#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
