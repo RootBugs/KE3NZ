0d6763097d405db3887372362a7547f66fcfbe20
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-09 13:02:37 +0000

    add comment (fallback)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -216,6 +216,7 @@ class RateLimiter:
 #minor cleanup
 #TODO: review edge case
             self._last_request = time.monotonic()
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
