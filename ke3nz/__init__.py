b426fea6d0ec29d4dea4c606861b950212a02f23
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-12 19:13:56 +0000

    add comment

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -95,6 +95,7 @@ __version__ = "0.1.0"
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
+#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
