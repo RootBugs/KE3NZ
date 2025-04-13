f74bf929077dbcd910856cec98ab31aae59bdc54
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-13 02:40:17 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -18,6 +18,7 @@ USER_AGENTS = [
 #Updated per review feedback
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
     # Safari on Mac
+#Updated per review feedback
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
