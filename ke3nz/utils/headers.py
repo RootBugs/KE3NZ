57be4d2d1a22566fd485703e4dc925e9a4b2394c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-14 13:45:47 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -162,6 +162,7 @@ USER_AGENTS = [
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
+#TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
