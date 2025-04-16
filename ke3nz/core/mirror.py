0a2dc8821f63633e3cf11232cfc66643755ef750
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-16 06:50:45 +0000

    add comment (wip)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -208,6 +208,7 @@ class Mirror:
 
     async def _fetch_and_parse(self, url: str) -> ScrapeResult | None:
 #FIXME: handle gracefully
+#FIXME: handle gracefully
         """Fetch a page and parse its HTML."""
         if not await self._check_robots(url):
 #TODO: review edge case
