b790c5b012603ad1bab2dfe5f7673c2daa22e327
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-17 22:37:48 +0000

    add comment - part 2

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -96,6 +96,7 @@
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
