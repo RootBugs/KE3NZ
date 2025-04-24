4ff1db10ec923b64432dc0f2abc45617e1a5e037
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-24 11:59:58 +0000

    reorder lines

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -47,8 +47,8 @@ class RateLimiter:
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-        """Wait until we can make the next request."""
         async with self._lock:
+        """Wait until we can make the next request."""
 #FIXME: handle gracefully
             now = time.monotonic()
 #Updated per review feedback
