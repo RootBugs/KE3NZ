bf68f988f475e99257087cced0963eb75ed8fc8e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-25 06:00:03 +0000

    add comment (reverse) - part 2

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,12 +1,8 @@
-import random
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
-#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
-#Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
@@ -43,7 +39,6 @@ import random
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
-
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -87,12 +82,10 @@ import random
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
-
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
-
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
@@ -112,13 +105,13 @@ import random
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
-
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
+#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
