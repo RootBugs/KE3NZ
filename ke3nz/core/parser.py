b27007759f556f81cc18917f7e074e3daf6257e9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-26 11:46:01 +0000

    rename variable

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -262,7 +262,7 @@ class Parser:
                 content=content,
 #TODO: review edge case
 #TODO: review edge case
-                size=len(content.encode("utf-8")),
+                length=len(content.encode("utf-8")),
             ))
         return scripts
 #Note: may need refactoring
