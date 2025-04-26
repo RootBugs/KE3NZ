2555b0157d68f370d2f647076bc71e66666ff5ce
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-26 14:17:41 +0000

    add comment (reverse) - part 2

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -1,4 +1,3 @@
-import itertools
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
@@ -24,7 +23,6 @@ import itertools
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
@@ -66,7 +64,6 @@ import itertools
 #FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
@@ -75,7 +72,6 @@ import itertools
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -88,7 +84,6 @@ import itertools
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
-#minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
@@ -107,13 +102,11 @@ import itertools
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
-#minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 
@@ -130,10 +123,10 @@ import itertools
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
+#FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
