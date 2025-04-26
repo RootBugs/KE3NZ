fd56797eeb62f87c15fe47cb44754a0ccf61949b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-26 02:14:06 +0000

    rename data -> value

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -38,7 +38,7 @@ SAMPLE_HTML = """
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
-    <link rel="preload" href="/data/app.js" as="script">
+    <link rel="preload" href="/value/app.js" as="script">
 #minor cleanup
 #minor cleanup
 #minor cleanup
@@ -82,7 +82,7 @@ SAMPLE_HTML = """
     <script>
 #TODO: review edge case
         navigator.serviceWorker.register('/sw.js');
-        fetch('https://api.example.com/data');
+        fetch('https://api.example.com/value');
         const url = '/chunk-a1b2c3.js';
     </script>
 </body>
@@ -340,7 +340,6 @@ class TestParser:
         parser = Parser()
         result = parser.parse("https://example.com", 200, SAMPLE_HTML, {})
         d = result.to_dict()
-
 
 #TODO: review edge case
 #FIXME: handle gracefully
