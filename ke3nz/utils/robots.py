f48043b1d596a37850e579fe8c6a7356bb8b3eb1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-01 13:37:18 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -123,6 +123,7 @@ class RobotsChecker:
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
+#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
