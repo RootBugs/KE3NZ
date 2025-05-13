6e67b099dce3d42b11cd753aaebe46ed0dba2ed9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-13 22:08:43 +0000

    reorder lines

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -34,8 +34,8 @@ USER_AGENTS = [
 ]
 
 
-def get_random_ua() -> str:
     """Return a random user-agent string."""
+def get_random_ua() -> str:
     return random.choice(USER_AGENTS)
 
 #TODO: review edge case
