04282e768d58047587c2b5d626cf8484b9801f21
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-13 18:43:53 +0000

    add comment (reverse) (wip)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,16 +1,10 @@
-import typing
-#minor cleanup
 #TODO: review edge case
-
-#Note: may need refactoring
-#Note: may need refactoring
-
-#FIXME: handle gracefully
-
-#Note: may need refactoring
-#FIXME: handle gracefully
-#minor cleanup
-#minor cleanup
-#Updated per review feedback
-#FIXME: handle gracefully
 #TODO: review edge case
+#Note: may need refactoring
+#Note: may need refactoring
+
+#FIXME: handle gracefully
+#Note: may need refactoring
+#minor cleanup
+#minor cleanup
+#FIXME: handle gracefully
