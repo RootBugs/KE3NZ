9be6ad2b4779661fb24026dad758608da20e010c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-16 18:22:31 +0000

    add comment (reverse) - part 2

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -7,6 +7,7 @@
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
+#Updated per review feedback
 #minor cleanup
 #minor cleanup
 
@@ -58,7 +59,6 @@ async def main():
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
-#minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
     # Example 1: Simple scrape
@@ -111,7 +111,6 @@ async def main():
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
-
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
@@ -168,7 +167,6 @@ async def main():
 #TODO: review edge case
         "https://example.com",
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
