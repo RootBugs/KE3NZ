f81c6b4af6b40e8157c26936ffc85c8b055b4571
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-17 12:52:51 +0000

    add comment (minor)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -51,6 +51,7 @@ class RobotsChecker:
 #Note: may need refactoring
 #FIXME: handle gracefully
             rp.allow_all = True
+#Updated per review feedback
 
         self._cache[robots_url] = rp
         return rp
