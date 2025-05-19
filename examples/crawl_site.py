8535618a1d46855c31c7d035c3398ab29ca7cb41
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-19 13:02:14 +0000

    add comment (fallback)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -9,6 +9,7 @@ from ke3nz import Crawler
 async def main():
     # Crawl example.com up to depth 2
     async with Crawler(delay=0.5) as c:
+#minor cleanup
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
