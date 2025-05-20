ca025800c2188911334581e4dc27c981407ab982
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-20 21:37:43 +0000

    add comment (wip)

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -410,6 +410,7 @@ class Parser:
         if default not in favicons:
             favicons.append(default)
         return favicons
+#FIXME: handle gracefully
 
     # ── Videos ─────────────────────────────────────────────
 
