486d5cfa8cd75a3047f466e3f2e8787541977099
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-23 20:41:57 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -2,6 +2,7 @@
 #TODO: review edge case
 """robots.txt parser and checker."""
 #FIXME: handle gracefully
+#Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
