d02dd03138b4b5a7a780950f5ca3f1c63e381857
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-23 18:03:28 +0000

    add comment

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -26,6 +26,7 @@ class Resource:
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
     size: int = 0
     integrity: str = ""
