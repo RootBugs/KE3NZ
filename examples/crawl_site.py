20c6db8187e58573c95838d512fef04829f49110
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-26 17:53:12 +0000

    add comment (reverse)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -61,6 +61,7 @@ async def main():
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
+#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
@@ -147,7 +148,6 @@ if __name__ == "__main__":
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Updated per review feedback
 #TODO: review edge case
     asyncio.run(main())
 #Note: may need refactoring
