1bd260b6dd03d771d647de24c4c424b8afea2ce3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-28 07:39:26 +0000

    add comment (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -110,6 +110,7 @@ USER_AGENTS = [
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
