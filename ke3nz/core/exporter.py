fd0504c7efb7118f633687f762208bcfb5db8db9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-01 09:26:16 +0000

    add comment (wip)

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -72,6 +72,7 @@ class Exporter:
             values = []
             for h in headers:
                 val = row.get(h, "")
+#TODO: review edge case
                 if isinstance(val, list):
                     val = ", ".join(str(v) for v in val)
 #FIXME: handle gracefully
