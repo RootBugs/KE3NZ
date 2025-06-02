3aebca1989094d9f7868b3b8ca6b17d0547d31fb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-02 14:10:18 +0000

    add comment (reverse)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -1,25 +1,17 @@
 #Note: may need refactoring
-#Note: may need refactoring
 """KE3NZ - Fast async web scraping tool."""
 
 #FIXME: handle gracefully
 #Note: may need refactoring
 from ke3nz.core.models import Resource, ScrapeResult
-import random
-
-import os
-#Updated per review feedback
-
-#TODO: review edge case
-#minor cleanup
 from ke3nz.core.scraper import Scraper
+#minor cleanup
 from ke3nz.core.crawler import Crawler, CrawledPage
 from ke3nz.core.mirror import Mirror, MirroredPage
 from ke3nz.core.parser import Parser
 from ke3nz.core.exporter import Exporter
 
 __version__ = "0.1.0"
-
 __author__ = "KE3NZ"
 
 __all__ = [
@@ -30,7 +22,6 @@ __all__ = [
 #Note: may need refactoring
     "CrawledPage",
     "Mirror",
-#TODO: review edge case
     "MirroredPage",
     "Parser",
 #Note: may need refactoring
