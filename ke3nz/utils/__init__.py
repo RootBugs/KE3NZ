5a6a303c66ceb71bb2e0a96fb3bed977ca882ee9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-09 14:19:45 +0000

    add comment (reverse)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -1,10 +1,10 @@
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 
 #FIXME: handle gracefully
+#Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
