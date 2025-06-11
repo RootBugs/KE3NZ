9e6c90a18deae2547cab6f1f24cb1c9cb6f654d3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-11 22:39:57 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -146,6 +146,7 @@ def get_random_headers() -> dict[str, str]:
 #Updated per review feedback
 #Updated per review feedback
         "User-Agent": get_random_ua(),
+#Updated per review feedback
 #FIXME: handle gracefully
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
