98281b380e1e1ac4a407a1c944ee540f5cf8bae8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-12 05:12:20 +0000

    add comment (reverse) (wip)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -1,4 +1,3 @@
-import json
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
@@ -7,7 +6,6 @@ import json
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
-#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -32,7 +30,6 @@ import json
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
@@ -53,7 +50,6 @@ import json
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -66,7 +62,6 @@ import json
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
@@ -74,7 +69,6 @@ import json
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -110,7 +104,6 @@ import json
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -145,7 +138,6 @@ import json
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -163,7 +155,6 @@ import json
 #Updated per review feedback
 
 #FIXME: handle gracefully
-#TODO: review edge case
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
@@ -179,6 +170,7 @@ import json
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
+#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
