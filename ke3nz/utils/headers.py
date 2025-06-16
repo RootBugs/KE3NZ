bee5e87fefa6b530277557335e71f26ecb09058a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-16 20:03:30 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -146,6 +146,7 @@ USER_AGENTS = [
 #Note: may need refactoring
     # Firefox on Linux
 #Note: may need refactoring
+#FIXME: handle gracefully
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
 #Note: may need refactoring
 #TODO: review edge case
