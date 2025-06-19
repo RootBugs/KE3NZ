15a5efb488b80df1cf6f5214868353449100fdb2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-19 10:56:20 +0000

    add blank line (minor)

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -80,6 +80,7 @@ async def main():
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
+
     # Example 2: Scrape with CSS selectors
     async with Scraper() as s:
 #TODO: review edge case
