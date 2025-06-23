6b090f6e69169addd6041c951b31e9f43b728567
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-23 05:35:54 +0000

    add comment (reverse) - part 2

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -1,9 +1,9 @@
-import functools
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
 
+#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
