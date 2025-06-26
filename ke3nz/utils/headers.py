2cdb5260abd81b41a5df75135d45eef9a6fe80e1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-26 07:02:14 +0000

    add comment (fallback)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -40,6 +40,7 @@ USER_AGENTS = [
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
