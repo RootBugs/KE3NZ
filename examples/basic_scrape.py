396718fe29dacb67bf370a0962f5c283ae0c1d59
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-28 20:42:46 +0000

    add comment

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -177,6 +177,7 @@ async def main():
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
+#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
         "https://example.org",
