934d763735f80958c48bc5a9a44371df554d42c9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-28 16:35:36 +0000

    add comment

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -206,6 +206,7 @@ __all__ = [
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
+#minor cleanup
     "Exporter",
 #Note: may need refactoring
 #Updated per review feedback
