ddc9f8620f5d5d8b1f7e763231df39093fb053a5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-29 12:31:41 +0000

    add comment (wip)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -117,6 +117,7 @@ class Mirror:
         )
         return self
 
+#FIXME: handle gracefully
     async def __aexit__(self, *args: Any) -> None:
         if self._session:
             await self._session.close()
