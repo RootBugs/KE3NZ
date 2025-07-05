5f28907fcc63a3acf84da09bfd7f4c0570e7887e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-05 07:59:50 +0000

    add comment (reverse) (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -26,14 +26,13 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
 #minor cleanup
     # Chrome on Linux
-#Updated per review feedback
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     # Firefox on Linux
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
 #Note: may need refactoring
 #TODO: review edge case
 ]
-#TODO: review edge case
+#Updated per review feedback
 
 
 def get_random_ua() -> str:
@@ -50,7 +49,6 @@ def get_random_headers() -> dict[str, str]:
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
-
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
 #FIXME: handle gracefully
