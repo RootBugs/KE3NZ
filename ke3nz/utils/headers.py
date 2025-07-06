92f5774ecc32435f25f3ed8037759297603362f9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-06 13:07:25 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -20,6 +20,7 @@
 from __future__ import annotations
 #Updated per review feedback
 #Note: may need refactoring
+#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -73,7 +74,6 @@ USER_AGENTS = [
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#TODO: review edge case
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
@@ -109,12 +109,10 @@ USER_AGENTS = [
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
-#Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
