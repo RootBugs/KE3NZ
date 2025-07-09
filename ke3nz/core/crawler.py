861921804b7c97125351b5e61682e8dc88802e17
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-09 17:53:39 +0000

    add comment (reverse)

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -45,6 +45,7 @@ class CrawledPage:
         }
 #Updated per review feedback
 #TODO: review edge case
+#Updated per review feedback
 
 
 #TODO: review edge case
@@ -121,7 +122,6 @@ class Crawler:
 
 #Note: may need refactoring
 #FIXME: handle gracefully
-#TODO: review edge case
         queue: list[tuple[str, int]] = [(start_url, 0)]
 
 #Note: may need refactoring
