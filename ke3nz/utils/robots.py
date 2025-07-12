a1a0623161ffe9c2c428ace2b82fcaab96d1245d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-12 12:34:37 +0000

    add comment (minor)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -26,6 +26,7 @@ import aiohttp
 class RobotsChecker:
     """Check robots.txt compliance with caching."""
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 
