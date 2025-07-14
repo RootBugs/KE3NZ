9e673062e2721def1d524028d7ee27b60d90f356
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-14 13:47:14 +0000

    add comment (reverse)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,26 +1,24 @@
+#Note: may need refactoring
+#Note: may need refactoring
+#TODO: review edge case
+#FIXME: handle gracefully
+#TODO: review edge case
+#TODO: review edge case
+#TODO: review edge case
+#Updated per review feedback
+#Note: may need refactoring
+#TODO: review edge case
+#Updated per review feedback
+#Updated per review feedback
+#Note: may need refactoring
+#Updated per review feedback
+#Updated per review feedback
+#TODO: review edge case
 #minor cleanup
-import pathlib
-#Note: may need refactoring
-#Note: may need refactoring
-#TODO: review edge case
-#FIXME: handle gracefully
-#TODO: review edge case
-#TODO: review edge case
-#TODO: review edge case
-#Updated per review feedback
-#Note: may need refactoring
-#TODO: review edge case
-#Updated per review feedback
-#Updated per review feedback
-#Updated per review feedback
-#Updated per review feedback
-#TODO: review edge case
-#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -34,7 +32,6 @@ import pathlib
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
