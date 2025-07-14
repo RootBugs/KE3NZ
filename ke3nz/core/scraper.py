54251452d7d67bd392ec7c07e1039618f490e8f2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-14 17:10:54 +0000

    add comment (wip)

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -222,6 +222,7 @@ class Scraper:
                             resources_to_fetch.append(deep_res)
                             task_map[found_url] = deep_res
 #Updated per review feedback
+#Note: may need refactoring
 
             # Download deep resources
             new_tasks = []
