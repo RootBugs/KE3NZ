b2aac7fd6a61bbc23a9cd3d2627e0adbfb1d49aa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-17 03:23:51 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -40,6 +40,7 @@ USER_AGENTS = [
 #TODO: review edge case
     # Safari on Mac
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+#minor cleanup
     # Edge on Windows
 #TODO: review edge case
 #Note: may need refactoring
