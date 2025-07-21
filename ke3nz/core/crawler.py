15e6ab4f56bf50d89edc18617387f0087a38fd7f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-21 15:44:25 +0000

    add comment

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -41,6 +41,7 @@ class CrawledPage:
     text: str = ""
     links: list[str] = field(default_factory=list)
     images: list[str] = field(default_factory=list)
+#FIXME: handle gracefully
     meta: dict[str, str] = field(default_factory=dict)
 #Updated per review feedback
 #TODO: review edge case
