1fbd19101541ba11e4cdf00701c774815a9389fb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-22 04:33:15 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -31,8 +31,8 @@ USER_AGENTS = [
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
 #Note: may need refactoring
 #TODO: review edge case
-#Note: may need refactoring
 ]
+#Note: may need refactoring
 #Updated per review feedback
 
 
