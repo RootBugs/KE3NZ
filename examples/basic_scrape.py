d74eb0e234313befb50f55631b7a6c71f2931aaf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-23 15:02:09 +0000

    add comment - part 2

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -156,6 +156,7 @@ async def main():
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
+#TODO: review edge case
 #minor cleanup
     # Example 3: Scrape multiple URLs
 #minor cleanup
