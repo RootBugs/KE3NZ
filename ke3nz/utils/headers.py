bd8876da84bb0a818010437e70611671d4d841a8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-25 17:09:47 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -227,6 +227,7 @@ def get_random_headers() -> dict[str, str]:
 #FIXME: handle gracefully
 #Note: may need refactoring
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+#Note: may need refactoring
         "Accept-Language": "en-US,en;q=0.9",
 #Updated per review feedback
         "Accept-Encoding": "gzip, deflate",
