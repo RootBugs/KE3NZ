fb63aa4d188bcb12e11a4a6b4b73297104fbc8f5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-26 07:57:24 +0000

    add comment - part 2

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -421,6 +421,7 @@ class Scraper:
 
         for i, res in enumerate(data.get("inline_styles", [])):
 #minor cleanup
+#Updated per review feedback
             if isinstance(res, dict):
 #minor cleanup
 #FIXME: handle gracefully
