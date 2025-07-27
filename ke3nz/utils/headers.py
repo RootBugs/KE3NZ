9db2782f966282d3f0dabe46e4d3b400e89ce373
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-27 17:12:09 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -121,6 +121,7 @@ def get_random_headers() -> dict[str, str]:
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
+#Note: may need refactoring
         "Upgrade-Insecure-Requests": "1",
 #TODO: review edge case
 #minor cleanup
