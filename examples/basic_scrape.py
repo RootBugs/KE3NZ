37180ec97ee1f3fb9def0bdea2e2e6be4d14d34a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-28 22:42:04 +0000

    add comment (reverse) (wip)

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -23,7 +23,6 @@
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -157,7 +156,6 @@ async def main():
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
     # Example 3: Scrape multiple URLs
 #minor cleanup
@@ -218,6 +216,7 @@ async def main():
 #Note: may need refactoring
 
 #minor cleanup
+#minor cleanup
 #TODO: review edge case
 
 #TODO: review edge case
@@ -240,7 +239,6 @@ if __name__ == "__main__":
 #minor cleanup
 #TODO: review edge case
     asyncio.run(main())
-#TODO: review edge case
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
