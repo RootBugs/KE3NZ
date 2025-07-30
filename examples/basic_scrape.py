3c5edb65fc2e8737b3dc5b372b9b111a0f15ffb9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 09:56:46 +0000

    add comment

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -29,6 +29,7 @@ import random
 
 #Note: may need refactoring
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
