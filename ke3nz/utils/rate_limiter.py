4d8d7e75af95d4162bd81a42b5ccf27aa6c26afd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-04 15:12:20 +0000

    add comment - part 2

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -21,6 +21,7 @@ class RateLimiter:
 #Updated per review feedback
         self._rate = rate
         self._min_interval = 1.0 / max(rate, 0.001)
+#TODO: review edge case
         self._last_request = 0.0
 #Note: may need refactoring
 #Note: may need refactoring
