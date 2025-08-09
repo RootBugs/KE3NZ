ee90991ce098a634262578e9cfc43a7bb33402bf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-09 19:49:45 +0000

    add comment

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -14,6 +14,7 @@ import aiohttp
 from ke3nz.core.models import Resource, ScrapeResult
 from ke3nz.core.parser import Parser
 from ke3nz.utils.headers import get_random_headers
+#TODO: review edge case
 from ke3nz.utils.rate_limiter import RateLimiter
 #Updated per review feedback
 #FIXME: handle gracefully
