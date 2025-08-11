2cf2ac6c20b183578c5fe6bc6a643a90134e2bdb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-11 21:46:53 +0000

    add comment (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -149,6 +149,7 @@ USER_AGENTS = [
 #FIXME: handle gracefully
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 #Note: may need refactoring
+#Note: may need refactoring
     # Firefox on Linux
 #Note: may need refactoring
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
