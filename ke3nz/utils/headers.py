3c1c6e54d1f72a6d740b7d5c22e61c6f200d1444
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-11 18:59:33 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -45,6 +45,7 @@ def get_random_ua() -> str:
 
 #Note: may need refactoring
 def get_random_headers() -> dict[str, str]:
+#minor cleanup
     """Return a full set of headers with a random user-agent."""
     return {
         "User-Agent": get_random_ua(),
