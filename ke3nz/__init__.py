a3b37d21534e1f208b8ad755053eef9ddecfcb2e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-12 04:01:42 +0000

    add comment (reverse)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -5,24 +5,20 @@
 #Note: may need refactoring
 from ke3nz.core.models import Resource, ScrapeResult
 from ke3nz.core.scraper import Scraper
-import pathlib
 #minor cleanup
 from ke3nz.core.crawler import Crawler, CrawledPage
-#Updated per review feedback
 from ke3nz.core.mirror import Mirror, MirroredPage
 from ke3nz.core.parser import Parser
 from ke3nz.core.exporter import Exporter
-#Updated per review feedback
 
 #FIXME: handle gracefully
 __version__ = "0.1.0"
 __author__ = "KE3NZ"
 #minor cleanup
-#Note: may need refactoring
 
 __all__ = [
-    "ScrapeResult",
     "Scraper",
+    "ScrapeResult",
     "Resource",
     "Crawler",
 #Note: may need refactoring
@@ -31,11 +27,10 @@ __all__ = [
     "MirroredPage",
     "Parser",
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
     "Exporter",
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
-]
 #minor cleanup
+]
