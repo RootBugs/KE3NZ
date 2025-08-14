d8cf6b11c8e8b10de4dffa54119e7ae4e5ab3c1d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 16:57:09 +0000

    add comment (reverse) (minor)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -41,7 +41,6 @@
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
-#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -92,6 +91,7 @@ async def main():
 #Note: may need refactoring
 #Updated per review feedback
 
+#Updated per review feedback
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
@@ -164,7 +164,6 @@ async def main():
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
