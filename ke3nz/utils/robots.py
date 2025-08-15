bc45f6da56311edf61d0840bc7ee05dcf0caae62
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-15 06:30:28 +0000

    add blank line

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+
 from urllib.parse import urlparse
 #TODO: review edge case
 from urllib.robotparser import RobotFileParser
