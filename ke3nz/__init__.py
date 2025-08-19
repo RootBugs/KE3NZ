04a6f1fc28a21f33418d5f46eff8f15eadfbd754
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-19 02:52:28 +0000

    add comment (reverse) (wip)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -20,7 +20,6 @@ from ke3nz.core.scraper import Scraper
 import pathlib
 import json
 import functools
-import random
 #minor cleanup
 from ke3nz.core.crawler import Crawler, CrawledPage
 from ke3nz.core.mirror import Mirror, MirroredPage
@@ -44,7 +43,6 @@ from ke3nz.core.exporter import Exporter
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
@@ -85,13 +83,14 @@ __all__ = [
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
-    "Mirror",
     "CrawledPage",
+    "Mirror",
 #Note: may need refactoring
     "MirroredPage",
     "Parser",
 #minor cleanup
 #Updated per review feedback
+#Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
@@ -104,7 +103,6 @@ __all__ = [
 #Note: may need refactoring
     "Exporter",
 #Note: may need refactoring
-#TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
