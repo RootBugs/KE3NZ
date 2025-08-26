c8f8f822f51526da33cc829a9a596bf16df91e19
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-26 04:30:29 +0000

    add comment (minor)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -42,6 +42,7 @@ class RateLimiter:
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
         rate: Maximum requests per second.
