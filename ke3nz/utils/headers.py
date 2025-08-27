fbee409241a295632162f41b7e186a23f774c5f2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-27 22:28:27 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -23,6 +23,7 @@ USER_AGENTS = [
     # Firefox on Mac
 #Updated per review feedback
     # Safari on Mac
+#minor cleanup
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
