3abec31c5b35bf339497857f3a0115353707ae07
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-27 03:29:24 +0000

    add blank line

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -14,6 +14,7 @@ async def main():
     # Crawl example.com up to depth 2
     async with Crawler(delay=0.5) as c:
 #TODO: review edge case
+
 #minor cleanup
 #TODO: review edge case
         pages = await c.crawl("https://example.com", max_depth=2)
