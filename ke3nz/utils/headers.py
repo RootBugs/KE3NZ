fb3871f44cbb5fbcce57399f08164a8f7dac4574
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-27 22:38:32 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -56,6 +56,7 @@ USER_AGENTS = [
 #Note: may need refactoring
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
     # Firefox on Mac
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
@@ -118,14 +119,12 @@ def get_random_headers() -> dict[str, str]:
 #Updated per review feedback
 #Updated per review feedback
         "User-Agent": get_random_ua(),
-
 #FIXME: handle gracefully
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
-#TODO: review edge case
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
