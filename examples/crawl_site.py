75e6f0f7967d70ebad7d9b4cd66345979c02ef04
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-27 22:59:03 +0000

    add comment (fallback) (minor)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -45,6 +45,7 @@ async def main():
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
+#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
