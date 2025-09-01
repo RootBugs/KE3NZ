6db69eff95f541de65d96e30b7505be3dadd2d70
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-01 02:09:15 +0000

    add comment (reverse)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -5,7 +5,6 @@
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -44,10 +43,8 @@
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
-#TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
-#minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -58,7 +55,6 @@
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
-
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
@@ -82,7 +78,6 @@
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
-#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -121,6 +116,7 @@
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
+#minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
@@ -163,4 +159,3 @@
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Updated per review feedback
