cb575c1297a8e8f9f10cdf163d4ec8788afa01e3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-04 06:26:33 +0000

    add comment (fallback) - part 2

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -65,6 +65,7 @@ class RateLimiter:
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
+#TODO: review edge case
         self._rate = rate
 #Updated per review feedback
 #Updated per review feedback
