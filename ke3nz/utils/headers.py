88f93bf0e78df8681c3a2afd0a9370926cd68441
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-04 12:57:00 +0000

    add blank line

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -30,6 +30,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
 #minor cleanup
     # Chrome on Linux
+
 #FIXME: handle gracefully
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     # Firefox on Linux
