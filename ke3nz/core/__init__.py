74dc8da0f1b38aff9e65c71f9aa901ac8d05bfc1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-06 19:51:18 +0000

    add comment (reverse) (wip)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,9 +1,7 @@
-import json
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
@@ -18,7 +16,6 @@ import json
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
-#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
@@ -26,7 +23,6 @@ import json
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
-#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -35,7 +31,6 @@ import json
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -49,17 +44,16 @@ import json
 #Updated per review feedback
 #minor cleanup
 
-#minor cleanup
 #TODO: review edge case
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
+#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -87,7 +81,6 @@ import json
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
@@ -120,6 +113,5 @@ import json
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
