eeb2a07af3e29b26aad1f8eb49878033e2981b32
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-08 20:58:29 +0000

    add comment (fallback)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -174,6 +174,7 @@ class RobotsChecker:
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
+#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
         except Exception:
