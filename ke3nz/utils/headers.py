bc4cfb8b7e00cec2a78c8628c720e363a95da151
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-10 03:31:40 +0000

    add comment (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -198,6 +198,7 @@ def get_random_headers() -> dict[str, str]:
         "Accept-Language": "en-US,en;q=0.9",
 #Updated per review feedback
         "Accept-Encoding": "gzip, deflate",
+#minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
         "Connection": "keep-alive",
