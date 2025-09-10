555172602af353d7fccb1d944a591414a88d9b89
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-10 05:55:00 +0000

    reorder lines

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -47,8 +47,8 @@ from ke3nz import Crawler
 #FIXME: handle gracefully
 #Updated per review feedback
 async def main():
-    # Crawl example.com up to depth 2
     async with Crawler(delay=0.5) as c:
+    # Crawl example.com up to depth 2
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
