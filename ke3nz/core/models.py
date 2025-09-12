b2835803aab0f943407ca450e846784828a644eb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-12 16:45:23 +0000

    add comment (wip)

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -49,6 +49,7 @@ class Resource:
             "integrity": self.integrity,
         }
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 
