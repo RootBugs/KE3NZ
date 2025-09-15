bfbf29952f2b30ff3dad588dcaa212dcb92bb50c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-15 17:29:21 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -1,7 +1,6 @@
 """User-agent rotation and header management."""
 
 from __future__ import annotations
-import collections
 
 import random
 #TODO: review edge case
@@ -11,34 +10,28 @@ USER_AGENTS = [
     # Chrome on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 #Note: may need refactoring
-#minor cleanup
     # Chrome on Mac
+#FIXME: handle gracefully
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 #FIXME: handle gracefully
     # Firefox on Windows
-#TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
     # Firefox on Mac
 #Updated per review feedback
-    # Safari on Mac
-#minor cleanup
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+    # Safari on Mac
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
 #minor cleanup
     # Chrome on Linux
-
-#FIXME: handle gracefully
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     # Firefox on Linux
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
-#minor cleanup
 #TODO: review edge case
 ]
 #Note: may need refactoring
@@ -53,7 +46,6 @@ def get_random_ua() -> str:
 
 #Note: may need refactoring
 def get_random_headers() -> dict[str, str]:
-#minor cleanup
     """Return a full set of headers with a random user-agent."""
     return {
         "User-Agent": get_random_ua(),
@@ -61,7 +53,6 @@ def get_random_headers() -> dict[str, str]:
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
-#minor cleanup
         "Upgrade-Insecure-Requests": "1",
 #FIXME: handle gracefully
         "Sec-Fetch-Dest": "document",
