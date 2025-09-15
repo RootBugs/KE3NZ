4cc647f29dfab5fb0342590e88018b0798b3b7a2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-15 04:09:58 +0000

    reorder lines - part 2

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -96,8 +96,8 @@ class Crawler:
 #Updated per review feedback
         self._visited: set[str] = set()
 
-    async def __aenter__(self) -> Crawler:
         await self._scraper.__aenter__()
+    async def __aenter__(self) -> Crawler:
         return self
 
     async def __aexit__(self, *args: Any) -> None:
