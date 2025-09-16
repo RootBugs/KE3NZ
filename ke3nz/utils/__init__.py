0a437a8679fcbe59df9c97585353c84ab0ba9ca6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-16 22:26:27 +0000

    add comment (reverse) (minor)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -38,14 +38,13 @@
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#TODO: review edge case
 #FIXME: handle gracefully
+#minor cleanup
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
 
@@ -54,7 +53,6 @@
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
-#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
