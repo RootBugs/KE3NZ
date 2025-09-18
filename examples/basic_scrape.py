3dd386bc7956f09ab45a76306af6a998fc6d2ff4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-18 12:27:41 +0000

    add comment

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -18,6 +18,7 @@ import random
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 async def main():
