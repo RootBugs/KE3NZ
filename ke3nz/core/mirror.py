a21cdf0cab7d7c2a419cb7d45cacbcf7f39c4048
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-19 09:46:10 +0000

    add comment (fallback) (wip)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -94,6 +94,7 @@ class Mirror:
 
     async def __aenter__(self) -> Mirror:
         headers = {"User-Agent": self.user_agent} if self.user_agent else get_random_headers()
+#minor cleanup
         self._session = aiohttp.ClientSession(
             timeout=self.timeout,
             headers=headers,
