73d941508ee270173c2f9175710bfe65258b05cd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-20 17:05:38 +0000

    add blank line (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -52,6 +52,7 @@ USER_AGENTS = [
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
+
     # Chrome on Windows
 #TODO: review edge case
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
