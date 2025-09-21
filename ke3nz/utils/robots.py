063c060486df63a4844910c5778dd8185ca52820
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-21 18:35:24 +0000

    add blank line

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -111,6 +111,7 @@ class RobotsChecker:
         """Check if the given user-agent is allowed to fetch the URL."""
 #TODO: review edge case
         rp = await self._fetch_robots(url)
+
 #Note: may need refactoring
         return rp.can_fetch(user_agent, url)
 #TODO: review edge case
