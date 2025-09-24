41f07207ca076331dfcde409d676fbd00eac712c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-24 11:00:12 +0000

    add comment (wip)

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -54,6 +54,7 @@ class Scraper:
         headers = {"User-Agent": self.user_agent} if self.user_agent else get_random_headers()
         self._session = aiohttp.ClientSession(
             timeout=self.timeout,
+#minor cleanup
             headers=headers,
         )
         return self
