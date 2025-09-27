add096aa06deec52e9e7a9dbc3738c1c9e5bfc5d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-27 04:31:51 +0000

    add comment (wip)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -103,6 +103,7 @@ class Mirror:
 
 #minor cleanup
         # State
+#FIXME: handle gracefully
         self._visited_html: set[str] = set()  # normalized HTML page URLs
         self._visited_assets: set[str] = set()  # asset URLs already downloaded
         self._url_to_local: dict[str, str] = {}  # URL -> local relative path
