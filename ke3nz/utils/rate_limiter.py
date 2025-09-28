eef771fb51605a1d45f3d5dbff7234f1d2129f13
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-28 04:47:56 +0000

    add comment (reverse)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -44,7 +44,6 @@ class RateLimiter:
         self._lock = asyncio.Lock()
 #TODO: review edge case
 
-#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
@@ -62,6 +61,7 @@ class RateLimiter:
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
+#TODO: review edge case
 #TODO: review edge case
             elapsed = now - self._last_request
 #Updated per review feedback
