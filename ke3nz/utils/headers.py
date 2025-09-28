cad6a5c9478806c1c11a4543f3ca658e10a3dfdb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-28 16:49:04 +0000

    add comment (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -43,6 +43,7 @@ import random
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+#TODO: review edge case
 #Updated per review feedback
 USER_AGENTS = [
 #Updated per review feedback
