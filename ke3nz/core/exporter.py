7acf1422ca3eacd39014ee46369e0824b8e6fb8d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-30 08:13:25 +0000

    add comment

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -38,6 +38,7 @@ class Exporter:
         # Flatten nested structures for CSV
         flat_data = []
 #minor cleanup
+#minor cleanup
         for row in data:
 #FIXME: handle gracefully
 #minor cleanup
