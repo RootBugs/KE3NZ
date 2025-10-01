d3157994de5c602b9252d4709ee7ad96ed22d34d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-01 06:24:06 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -11,6 +11,7 @@
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
+#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
