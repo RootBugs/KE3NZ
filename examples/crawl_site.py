d718a35fb8cb1962a23e8e4dfd08bd56da047e22
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-02 06:14:56 +0000

    add blank line (minor)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -34,6 +34,7 @@ async def main():
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
+
         pages = await c.crawl("https://example.com", max_depth=2)
 #minor cleanup
 #FIXME: handle gracefully
