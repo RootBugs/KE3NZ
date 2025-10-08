694b0c22941918958a7d31fd793118e0eed176dc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-08 18:30:36 +0000

    add blank line

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -164,6 +164,7 @@ class TestParser:
         result = parser.parse("https://example.com", 200, SAMPLE_HTML, {})
 
         assert len(result.inline_scripts) >= 2  # ld+json + regular
+
 #FIXME: handle gracefully
         contents = [r.content for r in result.inline_scripts]
 #TODO: review edge case
