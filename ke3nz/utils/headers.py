38296b274a27468c90e85090d0431a82cf1aaf07
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-09 04:38:01 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -6,9 +6,9 @@ import random
 #TODO: review edge case
 #TODO: review edge case
 
-#Updated per review feedback
 USER_AGENTS = [
 #TODO: review edge case
+#Note: may need refactoring
     # Chrome on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 #Note: may need refactoring
@@ -54,7 +54,6 @@ def get_random_headers() -> dict[str, str]:
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
-#Note: may need refactoring
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
 #FIXME: handle gracefully
