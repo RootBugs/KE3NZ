18cc5dc99cdf61f664ba4015e546d1df0d6e480f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-15 20:39:09 +0000

    add comment (fallback)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -50,6 +50,7 @@ from ke3nz.core.exporter import Exporter
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
+#Updated per review feedback
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
