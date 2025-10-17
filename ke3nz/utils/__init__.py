c799cd73e5e06a375d3f6a40f2a2622e630ba90a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-17 15:55:25 +0000

    add comment (reverse)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -1,17 +1,15 @@
-import itertools
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 
 #Updated per review feedback
-#minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#minor cleanup
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
