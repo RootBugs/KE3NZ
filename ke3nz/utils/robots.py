4d24237dec3ef5dfba5f0b706545466be41a50d2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-17 21:08:27 +0000

    add comment (wip)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -55,6 +55,7 @@ class RobotsChecker:
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
+#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
                 timeout=aiohttp.ClientTimeout(total=10)
