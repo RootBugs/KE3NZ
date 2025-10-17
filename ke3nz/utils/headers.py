a4074096a5bd6457ed98663c8e813a380cbd870c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-17 02:01:47 +0000

    reorder lines

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -157,8 +157,8 @@ def get_random_headers() -> dict[str, str]:
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
-        "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
+        "Connection": "keep-alive",
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
