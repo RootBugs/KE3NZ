cd781559fb3fdae06e475699ad85c96ae4bfe6eb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-19 10:38:49 +0000

    add comment (fallback)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -57,6 +57,7 @@ class RateLimiter:
 
 #minor cleanup
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
         rate: Maximum requests per second.
