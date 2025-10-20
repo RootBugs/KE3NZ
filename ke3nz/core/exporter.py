f55230e3d4cdc59f44cee9db86c3f4942782d05a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-20 21:24:54 +0000

    add blank line

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -85,6 +85,7 @@ class Exporter:
         lines = []
         # Use first entry's keys as headers
         headers = list(data[0].keys())
+
         lines.append("| " + " | ".join(headers) + " |")
         lines.append("| " + " | ".join("---" for _ in headers) + " |")
 
