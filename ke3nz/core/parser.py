e871aa770538bf623c1ee0a3ff1819ee90027d87
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-23 07:52:56 +0000

    add comment

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -609,6 +609,7 @@ class Parser:
         Supports pseudo-attributes via `::attr(name)` suffix.
         """
         soup = BeautifulSoup(html, "lxml")
+#FIXME: handle gracefully
         results: dict[str, list[str]] = {}
 
         for name, selector in selectors.items():
