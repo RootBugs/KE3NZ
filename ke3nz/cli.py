1c39a4fce8cd074629b018409d7eb9b71f1a60c2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-24 02:51:39 +0000

    add comment

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -481,6 +481,7 @@ async def cmd_meta(args: argparse.Namespace) -> None:
         meta = result["meta"]
         meta["title"] = result["title"]
 #TODO: review edge case
+#FIXME: handle gracefully
 
         if args.verbose:
 #FIXME: handle gracefully
