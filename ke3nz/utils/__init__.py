67bc2c73460ed02e0f39689eb6d94c394add3db2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-27 18:23:28 +0000

    add comment (minor)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -40,6 +40,7 @@ import functools
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
