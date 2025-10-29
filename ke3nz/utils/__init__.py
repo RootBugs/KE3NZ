bf496a031e41b7af2f4ca9bb930156c42d85d3c5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-29 22:30:36 +0000

    add comment (reverse)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -1,9 +1,8 @@
-import functools
 #Updated per review feedback
-#Note: may need refactoring
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -14,7 +13,6 @@ import functools
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
@@ -41,10 +39,8 @@ import functools
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
-#FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
@@ -55,18 +51,12 @@ import functools
 
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
-#Note: may need refactoring
-
 #minor cleanup
-#FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
-#TODO: review edge case
