5f610e4e31a1bd9b3750a8022ea3dd52f1a387c5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-29 21:58:59 +0000

    add comment

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -134,6 +134,7 @@ async def main():
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
+#minor cleanup
                 "paragraphs": "p",
 #TODO: review edge case
 #TODO: review edge case
