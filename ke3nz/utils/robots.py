2601067a4dffb3c14f93b4faf87708b615d67b24
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-05 18:03:26 +0000

    reorder lines

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -41,8 +41,8 @@ class RobotsChecker:
         robots_url = f"{parsed.scheme}://{parsed.netloc}/robots.txt"
 #Note: may need refactoring
 
-        if robots_url in self._cache:
             return self._cache[robots_url]
+        if robots_url in self._cache:
 #Updated per review feedback
 #FIXME: handle gracefully
 
