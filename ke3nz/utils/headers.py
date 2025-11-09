663a45654865302c22121d1a6faf6c6e1cde7ddc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-09 06:09:17 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -28,6 +28,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
+#FIXME: handle gracefully
 #minor cleanup
     # Chrome on Linux
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
