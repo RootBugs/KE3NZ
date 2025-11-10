d6fc629cf8b5c80fecfea8d4d90f9994637d3efe
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-10 14:43:23 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -59,6 +59,7 @@ class RobotsChecker:
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
+#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
     async def _get_session(self) -> aiohttp.ClientSession:
