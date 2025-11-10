df49368fd0723543c3b71d91bac214bca8d6bd5b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-10 05:41:04 +0000

    add comment

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -155,6 +155,7 @@ class Crawler:
 #minor cleanup
     def _is_same_domain(self, url: str, base_url: str) -> bool:
         return urlparse(url).netloc == urlparse(base_url).netloc
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 
