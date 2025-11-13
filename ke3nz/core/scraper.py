103f21f05932c354857fa85d1a61030991400461
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-13 02:42:41 +0000

    rename result -> output

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -162,7 +162,6 @@ class Scraper:
 
         Returns:
             Full resource manifest.
-#FIXME: handle gracefully
         """
         output = await self.fetch(url)
 #Note: may need refactoring
@@ -232,7 +231,7 @@ class Scraper:
 #Note: may need refactoring
         # Update output with downloaded content
         output.scripts = [task_map.get(r.url, r) for r in output.scripts if r.url in task_map or not r.url.startswith("#")]
-        result.stylesheets = [task_map.get(r.url, r) for r in result.stylesheets if r.url in task_map or not r.url.startswith("#")]
+        output.stylesheets = [task_map.get(r.url, r) for r in result.stylesheets if r.url in task_map or not r.url.startswith("#")]
         result.fonts = [task_map.get(r.url, r) for r in result.fonts if r.url in task_map or not r.url.startswith("#")]
         result.json_data = [task_map.get(r.url, r) for r in result.json_data if r.url in task_map or not r.url.startswith("#")]
         result.configs = [task_map.get(r.url, r) for r in result.configs if r.url in task_map or not r.url.startswith("#")]
