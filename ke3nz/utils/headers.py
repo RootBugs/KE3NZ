cb69686c152dcc66f52765fce8babebbc45a1dc8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-22 13:43:50 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -130,6 +130,7 @@ USER_AGENTS = [
     # Safari on Mac
 #Note: may need refactoring
 #Note: may need refactoring
+#Note: may need refactoring
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
 #Updated per review feedback
     # Edge on Windows
