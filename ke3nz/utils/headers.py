639583f74720ebd3328330999ac9b838ee9800a9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-22 16:57:15 +0000

    add comment - part 2

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -66,6 +66,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
 #TODO: review edge case
     # Safari on Mac
+#FIXME: handle gracefully
 #Note: may need refactoring
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
