4d0e87375041db43e05287313acab3565e319f5f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-22 10:41:42 +0000

    add comment (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -71,6 +71,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
 #TODO: review edge case
 #TODO: review edge case
+#Updated per review feedback
     # Firefox on Mac
 #Updated per review feedback
 #TODO: review edge case
