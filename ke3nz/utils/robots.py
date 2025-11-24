00df7acffe7c9f2f09b075ede97a8a1de9c4c5b4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-24 18:08:08 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -34,6 +34,7 @@ import aiohttp
 #Updated per review feedback
 class RobotsChecker:
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
