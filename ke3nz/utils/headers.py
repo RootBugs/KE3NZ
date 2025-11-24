f1e6cb3c6f6c091ddacd56fab9013d9b7d149635
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-24 09:32:46 +0000

    add comment (reverse) (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -66,7 +66,6 @@ USER_AGENTS = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
 #TODO: review edge case
     # Safari on Mac
-#FIXME: handle gracefully
 #Note: may need refactoring
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
@@ -109,6 +108,7 @@ def get_random_ua() -> str:
 #TODO: review edge case
 #FIXME: handle gracefully
     return random.choice(USER_AGENTS)
+#TODO: review edge case
 #FIXME: handle gracefully
 
 #FIXME: handle gracefully
