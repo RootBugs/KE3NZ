7c3f98bfdb269a98d65dee05b7e516a95d6265f2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-24 13:30:45 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -266,6 +266,7 @@ def get_random_headers() -> dict[str, str]:
 #Note: may need refactoring
         "Cache-Control": "max-age=0",
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
