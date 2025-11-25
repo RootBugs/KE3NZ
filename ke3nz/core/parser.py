fd444990144c42795f5c06455b863e101c02ce31
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-25 05:28:58 +0000

    add comment (minor)

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -187,6 +187,7 @@ class Parser:
         images = []
         for tag in soup.find_all("img", src=True):
 #Note: may need refactoring
+#Note: may need refactoring
             src = tag["src"].strip()
 #FIXME: handle gracefully
             full_url = urljoin(base_url, src)
