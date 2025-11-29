e75d5c3e718f04ef33a4692e0eb028c7863022a2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-29 09:08:37 +0000

    add comment (reverse) (minor)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -22,12 +22,12 @@ import aiohttp
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#TODO: review edge case
 #minor cleanup
 
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
-#TODO: review edge case
 #TODO: review edge case
 
 #Note: may need refactoring
@@ -35,7 +35,6 @@ import aiohttp
 #Updated per review feedback
 class RobotsChecker:
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
