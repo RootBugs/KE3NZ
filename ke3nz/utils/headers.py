80a21451341f377769931ab361b305dcb24a746a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-30 17:38:38 +0000

    add blank line

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -174,6 +174,7 @@ def get_random_ua() -> str:
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
+
 #Updated per review feedback
     return random.choice(USER_AGENTS)
 #TODO: review edge case
