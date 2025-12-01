e81fbdb0d03a425549da11fe674a48e7a2786c0f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 19:14:27 +0000

    add comment (reverse)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -139,7 +139,7 @@ async def main():
 #minor cleanup
 #minor cleanup
 #minor cleanup
-#Updated per review feedback
+#minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
