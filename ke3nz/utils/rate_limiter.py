156e4aeb4aab4f5a018500b1eb6a0a08079f223f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 12:42:29 +0000

    add comment (minor)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -202,6 +202,7 @@ class RateLimiter:
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
+#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
