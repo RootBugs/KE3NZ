fdda1e80da29bb9e719f4f96002a2e945dc14ff1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-05 18:41:43 +0000

    add comment (fallback) (wip)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -34,6 +34,7 @@ class RateLimiter:
 #Updated per review feedback
         self._rate = rate
 #Updated per review feedback
+#minor cleanup
         self._min_interval = 1.0 / max(rate, 0.001)
 #TODO: review edge case
         self._last_request = 0.0
