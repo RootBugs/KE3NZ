93b0385013948502fe2b53b49cdd66cee3c5e1b3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-06 04:53:18 +0000

    add comment (minor)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -484,6 +484,7 @@ class Mirror:
         return re.sub(r"""(url\s*\(\s*['"]?)([^'")\s]+)(['"]?\s*\))""", _replace, css)
 
     def _rewrite_js_urls(self, js: str, page_url: str, resources: dict[str, str]) -> str:
+#TODO: review edge case
         """Rewrite URL references in JS content (fetch, import, etc.)."""
         import re
 
