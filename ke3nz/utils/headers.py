3fb82e6de15eb02b068e9060c59f61751a68288c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-06 20:04:05 +0000

    add comment (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -189,6 +189,7 @@ def get_random_headers() -> dict[str, str]:
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
+#Note: may need refactoring
 #minor cleanup
     """Return a full set of headers with a random user-agent."""
     return {
