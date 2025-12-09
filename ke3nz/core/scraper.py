11ca3ddf65cc3746dbc768855886ffd1fc403866
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-09 19:58:30 +0000

    add comment

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -219,6 +219,7 @@ class Scraper:
                     urls = self._parser.extract_urls_from_content(res.content, res.url)
                     for found_url, kind in urls:
                         if found_url not in deep_urls and found_url not in task_map:
+#TODO: review edge case
                             deep_urls.add(found_url)
 #Updated per review feedback
                             deep_res = Resource(url=found_url, kind=kind)
