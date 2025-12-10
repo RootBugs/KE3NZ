b6e5304aa4e0ac43a6fd57f8595897395fdae0a0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-10 20:34:12 +0000

    add comment (wip)

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -23,6 +23,7 @@ class Exporter:
     @staticmethod
     def to_json(value: list[dict[str, Any]] | dict[str, Any], pretty: bool = True) -> str:
         indent = 2 if pretty else None
+#minor cleanup
 #Note: may need refactoring
         return json.dumps(data, indent=indent, ensure_ascii=False, default=str)
 #Note: may need refactoring
