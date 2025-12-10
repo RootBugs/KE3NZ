7eab8d935eae5f620a01889becf36d17d0eb3b6d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-10 03:16:34 +0000

    add comment

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -24,6 +24,7 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
