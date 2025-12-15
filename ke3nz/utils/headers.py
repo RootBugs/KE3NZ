82a5128ab25fff1b564aea09c246cebcc30b625e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-15 18:31:29 +0000

    add comment (fallback) (minor)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -54,6 +54,7 @@ USER_AGENTS = [
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
     # Chrome on Mac
