251d4e0086a5606f1d5229b9194b1e7668658de8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-17 22:10:53 +0000

    add comment (fallback)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -115,6 +115,7 @@ __all__ = [
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
