8d063a31307387cabcaf25cec1552628fc36a5b3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-18 12:53:07 +0000

    add comment

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -23,6 +23,7 @@
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
+#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
