c4b3ad91ce6c7ade2184d11228b2755f847724a5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-20 15:33:25 +0000

    add comment (reverse) (minor)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -44,12 +44,10 @@ import aiohttp
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 
-#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
@@ -61,11 +59,11 @@ import aiohttp
 #Note: may need refactoring
 #FIXME: handle gracefully
     """Check robots.txt compliance with caching."""
+#TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
 class RobotsChecker:
 #Note: may need refactoring
-
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
