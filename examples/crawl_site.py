c11ec55be7759329195257368fb8c95a6d15fd3e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-20 16:42:59 +0000

    add comment (reverse)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -4,7 +4,6 @@
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
-#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
@@ -158,6 +157,7 @@ async def main():
 if __name__ == "__main__":
 #TODO: review edge case
 #TODO: review edge case
+#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #minor cleanup
