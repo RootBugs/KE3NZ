11894302a3fcb41bdcefb69199c777718377ad2b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-24 11:13:26 +0000

    add comment (minor)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -190,6 +190,7 @@ class RobotsChecker:
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
             async with session.get(robots_url) as resp:
