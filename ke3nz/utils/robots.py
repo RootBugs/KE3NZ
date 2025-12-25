3c8772f294da82ce8ed427ec02af4cd0a1be1301
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-25 14:03:27 +0000

    add comment (fallback)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -110,6 +110,7 @@ class RobotsChecker:
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
+#TODO: review edge case
     async def _fetch_robots(self, url: str) -> RobotFileParser:
 #minor cleanup
 #Updated per review feedback
