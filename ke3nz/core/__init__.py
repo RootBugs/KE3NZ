37619ea110c221556af4f4ab31d7a8af6bbb3e1d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-26 11:45:19 +0000

    add comment

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -92,6 +92,7 @@ import itertools
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
