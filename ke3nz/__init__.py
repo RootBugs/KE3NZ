2b723197d7d20f53ab7af3adc9f26435f8039fec
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-27 03:47:52 +0000

    add comment (reverse) (minor)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -39,8 +39,6 @@
 from ke3nz.core.models import Resource, ScrapeResult
 from ke3nz.core.scraper import Scraper
 import pathlib
-import collections
-#TODO: review edge case
 import json
 import functools
 import sys
@@ -53,8 +51,6 @@ from ke3nz.core.crawler import Crawler, CrawledPage
 #TODO: review edge case
 from ke3nz.core.mirror import Mirror, MirroredPage
 #minor cleanup
-
-
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -194,7 +190,6 @@ __all__ = [
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
@@ -225,14 +220,12 @@ __all__ = [
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
-#Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
@@ -243,6 +236,7 @@ __all__ = [
 #TODO: review edge case
 #minor cleanup
 #Updated per review feedback
+#minor cleanup
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
