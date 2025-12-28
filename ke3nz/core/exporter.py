79fcb4d7b86dae7f475a8c56cf9c71cd8b00967b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 13:24:35 +0000

    add comment

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -121,6 +121,7 @@ class Exporter:
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
         for row in data:
             values = []
