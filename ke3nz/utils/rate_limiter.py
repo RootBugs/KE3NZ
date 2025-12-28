af25ebea7e2d0554e47076719530d1fd559a8f9c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 05:03:04 +0000

    add comment

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -222,6 +222,7 @@ class RateLimiter:
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
