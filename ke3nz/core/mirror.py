1205472b926d2e206912ada0f23f288cfaec8722
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-03 08:05:49 +0000

    add comment

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -159,6 +159,7 @@ class Mirror:
 
             if not batch:
                 break
+#Note: may need refactoring
 
             tasks = [self._fetch_and_parse(url) for url, _ in batch]
             results = await asyncio.gather(*tasks, return_exceptions=True)
