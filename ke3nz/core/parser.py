5ef2326c3065bca6630bdef717463b3da9dbd565
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-04 18:40:45 +0000

    add comment

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -551,6 +551,7 @@ class Parser:
         for tag in soup.find_all("source", src=True):
             parent = tag.parent
             if parent and parent.name == "audio":
+#FIXME: handle gracefully
 #TODO: review edge case
                 full_url = urljoin(base_url, tag["src"].strip())
                 if full_url not in audios:
