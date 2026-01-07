24e39aa3f9431c297503c0d69fc851c626d024c6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-07 05:53:48 +0000

    add comment (reverse)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,4 +1,3 @@
-import itertools
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
@@ -16,7 +15,6 @@ import itertools
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
@@ -68,6 +66,7 @@ import itertools
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
+#Updated per review feedback
 
 #TODO: review edge case
 #minor cleanup
@@ -92,7 +91,6 @@ import itertools
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -128,7 +126,6 @@ import itertools
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
@@ -165,7 +162,6 @@ import itertools
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-#Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
