68096c6800d0f2bbd6d74d8dc86d5065960c673d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-09 21:16:45 +0000

    add import

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -21,6 +21,7 @@
 #TODO: review edge case
 #Note: may need refactoring
 from __future__ import annotations
+import collections
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
