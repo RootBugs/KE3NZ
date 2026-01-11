14b66e0fb4fd0f2986b00ccebeee19261befd843
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-11 08:48:36 +0000

    add comment (reverse)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -64,7 +64,6 @@
 import asyncio
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
@@ -89,7 +88,6 @@ from ke3nz import Crawler
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
-#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
@@ -112,6 +110,7 @@ async def main():
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
+#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -164,7 +163,6 @@ async def main():
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -223,7 +221,6 @@ async def main():
 #minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
-#TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
