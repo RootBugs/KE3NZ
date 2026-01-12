817b99a242bf49b7b747f83a2700d4de36127a2f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-12 11:12:14 +0000

    add comment (wip)

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup
 
 from ke3nz.core.scraper import Scraper, ScrapeResult
 #FIXME: handle gracefully
+#minor cleanup
 from ke3nz.utils.headers import get_random_headers
 
 
