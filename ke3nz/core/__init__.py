7daa36ddefec900fef42748691b4767b949b6327
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-15 18:01:46 +0000

    add comment (reverse)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,8 +1,7 @@
-import random
-import sys
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
+#TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
@@ -25,7 +24,6 @@ import sys
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
-
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
