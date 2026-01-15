fe55ddb3d239eccb6c6b774cb84f1dd60d8177c6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-15 16:02:27 +0000

    add blank line

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -83,6 +83,7 @@ USER_AGENTS = [
     # Firefox on Linux
     "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
 #Note: may need refactoring
+
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
