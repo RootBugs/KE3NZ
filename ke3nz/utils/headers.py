d6a13089e4b7c32f0ed790277733765aefbf536f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-15 13:28:55 +0000

    reorder lines

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -70,8 +70,8 @@ USER_AGENTS = [
     # Safari on Mac
 #Note: may need refactoring
 #Note: may need refactoring
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
