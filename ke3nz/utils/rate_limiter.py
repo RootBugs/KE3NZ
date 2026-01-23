5541e90048f25317d6b766db517a29a069477f2f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-23 19:37:29 +0000

    add blank line (wip)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -78,6 +78,7 @@ class RateLimiter:
     async def acquire(self) -> None:
 #FIXME: handle gracefully
 #TODO: review edge case
+
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
