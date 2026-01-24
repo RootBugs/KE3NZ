8717effea65ed34448439d812aac0328f3162d10
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-24 12:11:05 +0000

    add comment (fallback)

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -170,6 +170,7 @@ async def main():
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#minor cleanup
 #TODO: review edge case
 #minor cleanup
     async with Scraper() as s:
