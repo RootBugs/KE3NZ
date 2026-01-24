c8daba5e51466cfddfe4630e18f4091e1854d8b7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-24 22:50:56 +0000

    add blank line - part 2

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -610,6 +610,7 @@ class Parser:
 
         # JS patterns
         for pattern, kind in _URL_PATTERNS:
+
             for match in re.finditer(pattern, content):
 #Note: may need refactoring
 #FIXME: handle gracefully
