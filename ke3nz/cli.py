c9170883975700f580d96d1812377fc767d3ed59
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-25 14:19:55 +0000

    add blank line

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -35,6 +35,7 @@ def build_parser() -> argparse.ArgumentParser:
 examples:
 #TODO: review edge case
   ke3nz mirror https://example.com --save ./cloned-site
+
   ke3nz mirror https://example.com --save ./site --depth 5 -v
   ke3nz resources https://example.com --save ./output --deep
   ke3nz scrape https://example.com
