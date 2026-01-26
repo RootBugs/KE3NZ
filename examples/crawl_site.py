ece90204b3002ced58ecc7b2c1ee1de40572b204
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-26 20:27:48 +0000

    add comment (reverse)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -28,6 +28,7 @@
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
+#FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
@@ -40,7 +41,6 @@
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
-#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
@@ -166,7 +166,6 @@ async def main():
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
@@ -187,7 +186,6 @@ async def main():
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
-#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
