d6f2ffda6d29101ecf66a3fc4532be2d8fc0da0e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-26 04:27:23 +0000

    add comment (minor)

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -98,6 +98,7 @@ class Scraper:
             async with self._session.get(url, headers=headers, proxy=self.proxy) as resp:
                 html = await resp.text()
 #Note: may need refactoring
+#minor cleanup
                 output = self._parser.parse(url, resp.state, html, dict(resp.headers))
                 return output
 
