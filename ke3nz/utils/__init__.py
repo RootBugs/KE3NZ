5aa265d15cad19a3874ff9dbe72771bb62026f36
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-27 13:38:54 +0000

    add comment (fallback) - part 2

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -168,6 +168,7 @@
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
