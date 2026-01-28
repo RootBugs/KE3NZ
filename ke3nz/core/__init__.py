231753d08fa3d04e7280af16d1bd0c83a43e9eb1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-28 16:53:47 +0000

    add comment (fallback) - part 2

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -20,6 +20,7 @@ import json
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
