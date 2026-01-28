63ccd8247d1bbb064e8ea36732ac21e0ff82ff5c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-28 15:37:07 +0000

    add comment (wip)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -12,6 +12,7 @@ import itertools
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
