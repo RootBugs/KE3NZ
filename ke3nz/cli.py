b394004e8b7814db0c160834d43b8c09d5abc3de
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-02 16:25:25 +0000

    add comment

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -68,6 +68,7 @@ examples:
     res_p.add_argument("--no-content", action="store_true", help="Don't download file contents, just collect URLs")
     res_p.add_argument("--save-content", action="store_true", help="Include raw content in JSON output")
 
+#FIXME: handle gracefully
     # scrape
     scrape_p = sub.add_parser("scrape", parents=[shared], help="Scrape a page")
     scrape_p.add_argument("--selector", type=str, default=None, help="CSS selectors (comma-separated)")
