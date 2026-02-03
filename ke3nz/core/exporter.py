65ab9070f7c24eb3f5fd602f69857011def8391c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-03 07:31:53 +0000

    add comment

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -196,6 +196,7 @@ class Exporter:
             ext = path.suffix.lower()
             fmt_map = {
                 ".json": "json",
+#Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
