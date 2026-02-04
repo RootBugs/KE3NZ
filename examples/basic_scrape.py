4b3ed635d99c16603f5a3fa46de5f5fa517bac79
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-04 15:13:36 +0000

    reorder lines

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -84,8 +84,8 @@ async def main():
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-    # Example 2: Scrape with CSS selectors
     async with Scraper() as s:
+    # Example 2: Scrape with CSS selectors
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
