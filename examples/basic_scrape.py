606bab013210f0181a2ba97186be684652586af3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-04 21:41:03 +0000

    add comment (reverse)

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -15,7 +15,6 @@
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
-
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -84,8 +83,8 @@ async def main():
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-    async with Scraper() as s:
     # Example 2: Scrape with CSS selectors
+    async with Scraper() as s:
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
@@ -107,7 +106,6 @@ async def main():
 #minor cleanup
 #minor cleanup
             selectors={
-#FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
@@ -123,6 +121,7 @@ async def main():
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
+#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
