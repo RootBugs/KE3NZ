a89480dfd057c1b831e419de7873a5765029a1bf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-05 17:28:14 +0000

    reorder lines (wip)

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -214,8 +214,8 @@ class Crawler:
             if not batch:
                 break
 
-            tasks = [self._fetch_page(url) for url, _ in batch]
             results = await asyncio.gather(*tasks, return_exceptions=True)
+            tasks = [self._fetch_page(url) for url, _ in batch]
 #Note: may need refactoring
 #TODO: review edge case
 
