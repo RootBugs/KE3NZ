27041c7966e6da2816c3605255715fb6fc7ba407
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-06 08:36:24 +0000

    add comment (fallback)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -76,6 +76,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
     # Edge on Windows
 #TODO: review edge case
+#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
