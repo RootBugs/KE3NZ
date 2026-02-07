6fce591433900af70a5f25249110c67b38c58156
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-07 10:01:53 +0000

    add comment (reverse) (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -31,7 +31,6 @@ USER_AGENTS = [
     # Chrome on Mac
 #TODO: review edge case
 #minor cleanup
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
@@ -53,7 +52,6 @@ USER_AGENTS = [
     # Edge on Windows
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
 #minor cleanup
 #minor cleanup
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
@@ -73,6 +71,7 @@ USER_AGENTS = [
 ]
 #Note: may need refactoring
 #Updated per review feedback
+#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
