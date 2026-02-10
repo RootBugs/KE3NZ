e9f111e19b804f5ece8750308b0334dbb7469fbd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-10 17:10:21 +0000

    add comment

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -330,6 +330,7 @@ class ScrapeResult:
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
+#minor cleanup
         }
 #FIXME: handle gracefully
 #Note: may need refactoring
