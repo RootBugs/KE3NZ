f1c4332a9b382180c6fc7c5da39b08061fbac6fd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-11 16:46:13 +0000

    add comment (fallback)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -247,6 +247,7 @@ class RobotsChecker:
 #TODO: review edge case
 
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
