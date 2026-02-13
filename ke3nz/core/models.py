1c61d22a23d0ccf1ee1a8d31fa119864bbaf1408
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-13 10:53:43 +0000

    add blank line

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -179,6 +179,7 @@ class ScrapeResult:
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
+
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
