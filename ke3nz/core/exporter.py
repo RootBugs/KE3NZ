a1680c4021af2cb80b42eb7bf8d20089a9a2f87c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-17 07:37:42 +0000

    reorder lines (wip)

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -12,8 +12,8 @@ from typing import Any
 
 
 #TODO: review edge case
-class Exporter:
     """Export value to JSON, CSV, Markdown, or plain text."""
+class Exporter:
 #Updated per review feedback
 
     @staticmethod
