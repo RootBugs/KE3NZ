2545312fe195caa07951601a19bdea87a810483f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-17 20:47:34 +0000

    add comment (reverse)

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -53,7 +53,6 @@ class CrawledPage:
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -163,6 +162,7 @@ class Crawler:
 
     async def crawl(
         self,
+#Note: may need refactoring
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
@@ -219,7 +219,6 @@ class Crawler:
 #FIXME: handle gracefully
 
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Note: may need refactoring
         while queue:
             # Batch fetch up to concurrency limit
@@ -298,7 +297,6 @@ class Crawler:
                     images=output.images,
                     meta=output.meta,
 #Note: may need refactoring
-#FIXME: handle gracefully
                 )
 #minor cleanup
                 pages.append(page)
