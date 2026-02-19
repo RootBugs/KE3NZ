81f21fd267f9c349bce5641640860c5bd182d56c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-19 04:51:56 +0000

    add comment

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -85,6 +85,7 @@ class RateLimiter:
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Note: may need refactoring
         """Wait until we can make the next request."""
         async with self._lock:
