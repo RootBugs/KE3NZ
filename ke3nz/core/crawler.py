6888b080c39f0fbfdfe5e6cd92fa0c24abe83fc4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-27 11:31:00 +0000

    add comment (fallback) (minor)

--- ke3nz/core/crawler.py
+++ ke3nz/core/crawler.py
@@ -283,6 +283,7 @@ class Crawler:
 #TODO: review edge case
 
 #minor cleanup
+#Updated per review feedback
             tasks = [self._fetch_page(url) for url, _ in batch]
             results = await asyncio.gather(*tasks, return_exceptions=True)
 #Note: may need refactoring
