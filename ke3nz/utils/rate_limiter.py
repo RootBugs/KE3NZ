66adb313f1686746b36393c88f8b38e7d84ef347
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-02 18:33:06 +0000

    add comment (fallback) (minor)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -139,6 +139,7 @@ class RateLimiter:
 #Note: may need refactoring
             if elapsed < self._min_interval:
 #Updated per review feedback
+#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
