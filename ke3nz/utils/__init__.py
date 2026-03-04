56aa20f8521a6526024970859189c6a6cbff18d3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-04 12:42:20 +0000

    add comment (minor)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -46,6 +46,7 @@
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
+#FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
