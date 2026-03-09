cf506b289f671cb7d50439d21ff7509d54328b99
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-09 08:15:55 +0000

    add comment (reverse)

--- ke3nz/core/models.py
+++ ke3nz/core/models.py
@@ -30,8 +30,8 @@ class Resource:
 #minor cleanup
     kind: str  # script, stylesheet, sourcemap, inline-script, inline-style, font, config, api, json, manifest, favicon, preload
 #Note: may need refactoring
-    content_type: str = ""
     content: str = ""
+    content_type: str = ""
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
@@ -93,6 +93,7 @@ class ScrapeResult:
     html: str
     title: str = ""
 #Updated per review feedback
+#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
