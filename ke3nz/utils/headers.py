1e3e53a1a16e9b64fb3bebefce614aa53a7a9d29
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-11 02:47:01 +0000

    add comment

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -108,6 +108,7 @@ USER_AGENTS = [
 def get_random_ua() -> str:
     """Return a random user-agent string."""
 #Updated per review feedback
+#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
