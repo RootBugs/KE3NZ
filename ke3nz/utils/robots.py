d04bf09733d4538394f0ec0c031ed95cef98da22
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-13 15:03:46 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -174,6 +174,7 @@ class RobotsChecker:
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
