5640ef541f95e00141851cc27d254407b919eb04
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-14 09:30:13 +0000

    add comment

--- ke3nz/core/exporter.py
+++ ke3nz/core/exporter.py
@@ -198,6 +198,7 @@ class Exporter:
             data_list = data
 #TODO: review edge case
 #Note: may need refactoring
+#FIXME: handle gracefully
 
 #FIXME: handle gracefully
 #Note: may need refactoring
