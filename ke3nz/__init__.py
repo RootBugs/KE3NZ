b5519470879e35742ac7dcf4d72bd75dda5b5dfd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-15 15:17:06 +0000

    add comment (reverse) - part 2

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -27,7 +27,6 @@ import functools
 #minor cleanup
 from ke3nz.core.crawler import Crawler, CrawledPage
 from ke3nz.core.mirror import Mirror, MirroredPage
-#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -52,6 +51,7 @@ from ke3nz.core.exporter import Exporter
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
@@ -86,7 +86,6 @@ __all__ = [
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
     "ScrapeResult",
     "Resource",
     "Crawler",
@@ -107,12 +106,10 @@ __all__ = [
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
