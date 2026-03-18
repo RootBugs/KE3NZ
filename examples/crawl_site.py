074586695b7522a0728559f47ba1ce0c9bd64f95
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-18 16:45:51 +0000

    add comment (fallback)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -39,6 +39,7 @@ async def main():
 #TODO: review edge case
         pages = await c.crawl("https://example.com", max_depth=2)
 #minor cleanup
+#Note: may need refactoring
 #FIXME: handle gracefully
 
 #minor cleanup
