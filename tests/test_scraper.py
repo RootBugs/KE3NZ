b654ca931157495ecdc338f47840829f86a53b15
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-22 13:02:02 +0000

    add comment (wip)

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -112,6 +112,7 @@ class TestParser:
 #minor cleanup
         result = parser.parse("https://example.com", 200, SAMPLE_HTML, {})
 
+#TODO: review edge case
 #FIXME: handle gracefully
         assert "https://example.com/page1" in result.links
         assert "https://example.com/page2" in result.links
