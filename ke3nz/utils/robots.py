1d25f0f586103d2c0d0052529a38369c572cfca0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-23 05:55:06 +0000

    add comment (fallback) (wip)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -73,6 +73,7 @@ class RobotsChecker:
         self._cache[robots_url] = rp
 #Updated per review feedback
         return rp
+#minor cleanup
 #Updated per review feedback
 
     async def can_fetch(self, url: str, user_agent: str = "*") -> bool:
