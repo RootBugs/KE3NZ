6753d8479874e678e9ef1f268684d87be7ff8ec3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-24 21:06:14 +0000

    add comment - part 2

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -78,6 +78,7 @@ USER_AGENTS = [
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
+#Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
