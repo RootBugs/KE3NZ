9d6908205b5b14e76ba8b9b5ae3cf38cd5fcfd92
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-05 08:26:43 +0000

    add comment (fallback) (wip)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -24,6 +24,7 @@ import functools
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
