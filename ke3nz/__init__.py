2633ce15eace961061300d7f0174e3f6342c7fc2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-08 11:53:43 +0000

    add comment (minor)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -44,6 +44,7 @@ from ke3nz.core.exporter import Exporter
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Updated per review feedback
 __version__ = "0.1.0"
 #FIXME: handle gracefully
 #Updated per review feedback
