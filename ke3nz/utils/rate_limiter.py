7722de4009196159f0da1286b2ad7f4598e753aa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-10 21:02:12 +0000

    add comment

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -88,6 +88,7 @@ class RateLimiter:
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
         self._lock = asyncio.Lock()
 #FIXME: handle gracefully
