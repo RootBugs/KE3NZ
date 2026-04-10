688e2aa26c900148d0211a637680a4f05f35dc7c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-10 18:59:02 +0000

    add comment (reverse) (minor)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -1,4 +1,3 @@
-import os
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
@@ -19,27 +18,26 @@ import os
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
+#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
+#minor cleanup
+#Note: may need refactoring
+#Note: may need refactoring
+#FIXME: handle gracefully
+#TODO: review edge case
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#Note: may need refactoring
+#Updated per review feedback
+#Note: may need refactoring
+#Note: may need refactoring
+#minor cleanup
 
 #minor cleanup
-#Note: may need refactoring
-#Note: may need refactoring
-#FIXME: handle gracefully
-#TODO: review edge case
-#FIXME: handle gracefully
-#FIXME: handle gracefully
-#FIXME: handle gracefully
-#Note: may need refactoring
-#Updated per review feedback
-#Note: may need refactoring
-#Note: may need refactoring
-#minor cleanup
-
-#minor cleanup
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
@@ -82,7 +80,6 @@ import os
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
@@ -90,6 +87,5 @@ import os
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
