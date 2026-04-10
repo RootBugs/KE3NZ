5ad8de2ac3e8f93531f0f8032560c912dfa2cc30
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-10 09:19:34 +0000

    add blank line

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -168,6 +168,7 @@ def get_random_headers() -> dict[str, str]:
         "User-Agent": get_random_ua(),
 #FIXME: handle gracefully
 #Note: may need refactoring
+
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
