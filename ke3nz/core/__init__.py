7ba5924805e0b8f04b3637bbaee5b6b70ce121a1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-13 11:52:26 +0000

    add comment (minor)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -13,6 +13,7 @@
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
+#Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
