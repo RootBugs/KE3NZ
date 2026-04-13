e353535a1a18e3a9b0151f596a99a3876ec6dc29
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-13 06:18:34 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -19,13 +19,12 @@
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
+#minor cleanup
 #Note: may need refactoring
 from __future__ import annotations
-import collections
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
-#FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
