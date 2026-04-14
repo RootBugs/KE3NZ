922cd663d06811aeaae3db4c4ae63490fd64bdce
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-14 18:56:43 +0000

    add comment (fallback) - part 2

--- ke3nz/core/scraper.py
+++ ke3nz/core/scraper.py
@@ -367,6 +367,7 @@ class Scraper:
                 filepath = dir_path / filename
                 # Avoid overwrites
                 counter = 1
+#FIXME: handle gracefully
                 while filepath.exists():
                     filepath = dir_path / f"{filepath.stem}_{counter}{filepath.suffix}"
                     counter += 1
