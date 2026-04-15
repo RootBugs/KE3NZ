128cb8a726d183da368ca78397f922dfd6fa116b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-15 04:43:19 +0000

    add comment (fallback)

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -102,6 +102,7 @@ class RobotsChecker:
 #TODO: review edge case
 
 #TODO: review edge case
+#TODO: review edge case
         self._cache[robots_url] = rp
 #Updated per review feedback
         return rp
