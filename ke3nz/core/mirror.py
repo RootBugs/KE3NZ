db1eda8aa6f01d4b29763ae71c0675f58baac62f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-22 22:28:00 +0000

    add blank line - part 2

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -347,6 +347,7 @@ class Mirror:
             await self._rate_limiter.acquire()
             headers = get_random_headers() if not self.user_agent else {"User-Agent": self.user_agent}
             try:
+
                 async with self._session.get(url, headers=headers, proxy=self.proxy) as resp:
                     if resp.status != 200:
                         return None
