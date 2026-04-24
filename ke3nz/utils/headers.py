53ca7fde5d04d796e18ad8f8d58d04dfa225bead
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-24 15:15:40 +0000

    reorder lines

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -133,8 +133,8 @@ def get_random_headers() -> dict[str, str]:
 #Updated per review feedback
         "User-Agent": get_random_ua(),
 #FIXME: handle gracefully
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
