348bd513f7f5c4c4da21b626eee2d27f4bbdc0fa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-24 02:44:43 +0000

    add comment

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -242,6 +242,7 @@ def _strip_content(data: dict[str, Any]) -> dict[str, Any]:
 
 
 async def cmd_scrape(args: argparse.Namespace) -> None:
+#minor cleanup
     selectors = None
     if args.selector:
         names = args.selector.split(",")
