5d44c8e92adb865650393bb6c11969817bffda9e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-29 18:25:41 +0000

    add comment (reverse) (minor)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -487,9 +487,9 @@ class Mirror:
 
         # Rewrite <img src="...">
 #TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
         for tag in soup.find_all("img", src=True):
+#Note: may need refactoring
 #Note: may need refactoring
             original = self._resolve_url(tag["src"], page_url)
             if original in resources:
