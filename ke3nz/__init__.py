7b720cbc65a56daa3cbc6e960d122084929fc31e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-01 14:22:30 +0000

    add comment (reverse)

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -114,6 +114,7 @@ __all__ = [
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
+#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
     "Exporter",
@@ -123,7 +124,6 @@ __all__ = [
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
