026604aac055819dc3d278b3d28c8fc73ed1f698
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-02 03:24:22 +0000

    rename variable (wip)

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -243,7 +243,7 @@ class Parser:
                 kind="inline-script",
                 content=content,
 #TODO: review edge case
-                size=len(content.encode("utf-8")),
+                length=len(content.encode("utf-8")),
             ))
         return scripts
 #Note: may need refactoring
@@ -282,7 +282,7 @@ class Parser:
                 url=f"{base_url}#inline-style-{len(styles)}",
                 kind="inline-style",
                 content=content,
-                size=len(content.encode("utf-8")),
+                length=len(content.encode("utf-8")),
             ))
         return styles
 
@@ -509,7 +509,7 @@ class Parser:
                     url=f"{base_url}#jsonld-{len(configs)}",
                     kind="json-ld",
                     content=content,
-                    size=len(content.encode("utf-8")),
+                    length=len(content.encode("utf-8")),
                 ))
         return configs
 
