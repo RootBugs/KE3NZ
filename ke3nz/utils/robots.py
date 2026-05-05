802786385b44a79cc35684ce1683475c76448ec5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-05 07:40:43 +0000

    add comment

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -121,6 +121,7 @@ class RobotsChecker:
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
+#Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
