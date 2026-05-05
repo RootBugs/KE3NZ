d9c95b1a26b2b2190410908de713e0bf4b2b2668
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-05 15:57:20 +0000

    add comment

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -45,6 +45,7 @@
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
