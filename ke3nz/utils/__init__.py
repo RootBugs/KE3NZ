93a91380f50b2195923ab0f9e65f5a04395be27f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-11 09:43:01 +0000

    add comment (fallback)

--- ke3nz/utils/__init__.py
+++ ke3nz/utils/__init__.py
@@ -17,6 +17,7 @@
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
