c9854f7d7f766c85316cb9005e751663e5ff20c7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-12 06:55:43 +0000

    add comment (wip)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -68,6 +68,7 @@ class RateLimiter:
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
