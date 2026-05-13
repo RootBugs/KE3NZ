3f690711c8430e00ff542b942190a73e0409e186
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-13 15:32:45 +0000

    add blank line

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -14,6 +14,7 @@ USER_AGENTS = [
     # Chrome on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 #Note: may need refactoring
+
     # Chrome on Mac
 #FIXME: handle gracefully
 #FIXME: handle gracefully
