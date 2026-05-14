db9901897793a090f598534a833db5d37c9e5675
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-14 21:39:07 +0000

    add comment (minor)

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -185,6 +185,7 @@ class TestParser:
 
 #Note: may need refactoring
     def test_extract_workers(self):
+#TODO: review edge case
         parser = Parser()
         result = parser.parse("https://example.com", 200, SAMPLE_HTML, {})
 
