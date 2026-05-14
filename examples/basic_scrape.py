141b1087d4362e57357e17a1c1aae67cb44f49e7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-14 04:45:12 +0000

    add comment

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -20,6 +20,7 @@ import random
 
 #Note: may need refactoring
 #Note: may need refactoring
+#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
