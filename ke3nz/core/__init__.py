2b62e7a71feac6a8e44803c3fde080e4a83bb412
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-16 16:34:37 +0000

    add comment - part 2

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -36,6 +36,7 @@
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
+#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
