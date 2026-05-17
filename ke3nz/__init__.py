ae022684bf1d5106e5d7b4aacc8034d3fcc6b1c5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-17 17:49:46 +0000

    add comment (fallback) - part 2

--- ke3nz/__init__.py
+++ ke3nz/__init__.py
@@ -20,6 +20,7 @@ from ke3nz.core.exporter import Exporter
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 __version__ = "0.1.0"
 __author__ = "KE3NZ"
