1c00b62d7ecc886622c59cbbfae41753f571f2e8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-17 14:38:41 +0000

    add comment (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -235,6 +235,7 @@ def get_random_headers() -> dict[str, str]:
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
+#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
