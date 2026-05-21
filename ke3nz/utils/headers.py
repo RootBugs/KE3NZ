b0262d58e9c3d2f10eab00745d7c8c954e1c902a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-21 12:35:24 +0000

    add comment (reverse)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -17,6 +17,7 @@
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
+#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
@@ -235,7 +236,6 @@ def get_random_headers() -> dict[str, str]:
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
-#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
