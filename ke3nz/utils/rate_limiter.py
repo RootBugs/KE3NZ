7d7f92001a1694beea910b36210d237852dc8b7f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-25 09:45:41 +0000

    add blank line

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -45,6 +45,7 @@ class RateLimiter:
 #TODO: review edge case
         self._last_request = 0.0
 #Note: may need refactoring
+
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
