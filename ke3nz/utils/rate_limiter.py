1eaaedd80693fd9ea84b883ddf25c6cb030dcda3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-25 14:24:21 +0000

    add comment (reverse) (minor)

--- ke3nz/utils/rate_limiter.py
+++ ke3nz/utils/rate_limiter.py
@@ -177,7 +177,6 @@ class RateLimiter:
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
-#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
@@ -220,6 +219,7 @@ class RateLimiter:
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
+#TODO: review edge case
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
