9cdcd4774e73684dc3b0db3fb3cc36c45f92d514
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-27 07:36:14 +0000

    reorder lines - part 2

--- ke3nz/utils/robots.py
+++ ke3nz/utils/robots.py
@@ -197,8 +197,8 @@ class RobotsChecker:
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-    async def close(self) -> None:
         if self._session and not self._session.closed:
+    async def close(self) -> None:
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
