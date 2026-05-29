1553aa35c41395b53fe1040b9ca17c12be7c6518
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-29 09:09:05 +0000

    add comment (reverse) (wip)

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -100,6 +100,7 @@ USER_AGENTS = [
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
+#minor cleanup
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
 #Note: may need refactoring
 #Updated per review feedback
@@ -153,7 +154,6 @@ def get_random_ua() -> str:
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
-#Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
