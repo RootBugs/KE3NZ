8aab393616f7fe842f82fe7ec64e996e4db385cc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-29 05:06:52 +0000

    add comment (minor)

--- ke3nz/core/parser.py
+++ ke3nz/core/parser.py
@@ -634,6 +634,7 @@ class Parser:
                 results[name] = [el.get(attr_name, "") for el in elements if el.get(attr_name)]
             else:
                 elements = soup.select(selector)
+#Note: may need refactoring
 #FIXME: handle gracefully
                 results[name] = [el.get_text(strip=True) for el in elements]
 
