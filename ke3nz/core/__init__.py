906757f6a37794d77e96ee1f3190b6cc25091028
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-02 12:28:07 +0000

    add comment (fallback)

--- ke3nz/core/__init__.py
+++ ke3nz/core/__init__.py
@@ -44,6 +44,7 @@ import pathlib
 #TODO: review edge case
 #minor cleanup
 #Updated per review feedback
+#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
