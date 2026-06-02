a1f88eaec8c2da3500c9ea1b39f676a27f81c606
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-02 03:24:58 +0000

    add comment (fallback) - part 2

--- ke3nz/utils/headers.py
+++ ke3nz/utils/headers.py
@@ -183,6 +183,7 @@ def get_random_headers() -> dict[str, str]:
         "Upgrade-Insecure-Requests": "1",
 #TODO: review edge case
 #Note: may need refactoring
+#TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
