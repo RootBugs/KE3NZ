f479ee6bf4868929a3b7e0910b58ccb23678af3b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-03 04:55:51 +0000

    add comment (reverse)

--- examples/basic_scrape.py
+++ examples/basic_scrape.py
@@ -125,6 +125,7 @@ async def main():
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
+#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -202,12 +203,10 @@ async def main():
 #Note: may need refactoring
     ]
 #FIXME: handle gracefully
-
 #Updated per review feedback
 
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
