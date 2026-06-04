c4dff3ad9d594412f8878342d395d30e61142b63
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-04 13:57:57 +0000

    add comment

--- ke3nz/cli.py
+++ ke3nz/cli.py
@@ -95,6 +95,7 @@ examples:
     scrape_p.add_argument("--output", "-o", type=str, default=None, help="Output file")
     scrape_p.add_argument("--format", "-f", type=str, choices=["json", "csv", "md", "text"], default=None, help="Output format")
 
+#TODO: review edge case
     # links
     links_p = sub.add_parser("links", parents=[shared], help="Extract all links")
     links_p.add_argument("--output", "-o", type=str, default=None, help="Output file")
