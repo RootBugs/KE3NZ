d16fd697afc1a0afb0a1fc46e370c78786aa5647
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-08 16:51:57 +0000

    add comment (reverse) (minor)

--- examples/crawl_site.py
+++ examples/crawl_site.py
@@ -43,7 +43,6 @@ async def main():
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
         pages = await c.crawl("https://example.com", max_depth=2)
 #minor cleanup
 #FIXME: handle gracefully
@@ -63,6 +62,7 @@ async def main():
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
