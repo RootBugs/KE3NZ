0b238258e0cab3b6cbedc57d5d807963ee308484
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-10 09:37:16 +0000

    add blank line (wip)

--- ke3nz/core/mirror.py
+++ ke3nz/core/mirror.py
@@ -195,6 +195,7 @@ class Mirror:
 
 #Note: may need refactoring
         # Phase 2: Save manifest + README
+
         self._save_manifest(base, start_url)
         self._save_readme(base, start_url)
 
